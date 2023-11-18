15596ded0b38554baac5d76f20b5a6c570944236
Valeryia Andraichuk <andraichuk.valeryia@gmail.com>
2023-11-18 10:09:22 +0100

    Initialize Swift project

---
+++ TasteCraft/TasteCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasteCraftApp.swift
+//  TasteCraft
+//
+//  Created by Valeryia Andraichuk on 18.11.23.
+//
+
+import SwiftUI
+
+@main
+struct TasteCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
