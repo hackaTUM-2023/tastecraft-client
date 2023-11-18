276782ded643c58a81c82433701c331c196606c7
Valeryia Andraichuk <andraichuk.valeryia@gmail.com>
2023-11-18 23:24:01 +0100

    Add networking client

---
+++ TasteCraft/Networking/NetworkError.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkError.swift
+//  TasteCraft
+//
+//  Created by Valeryia Andraichuk on 18.11.23.
+//
+
+import Foundation
+
+public enum NetworkError: Error {
+    case invalidResponse
+}
